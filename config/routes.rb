fdc9c141aa16d60017fdb688f7b4bbe8d928e722
EC2 Default User <ec2-user@ip-172-31-44-206.ap-northeast-1.compute.internal>
2023-03-11 09:01:58 +0000

    [Update]

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+
+devise_for :users, skip:[:passwords], controllers:{
+    registrations: "public/registrations",
+    sessions: 'user/sessions'
+  }
+ devise_for :admin, skip:[:registrations, :passwords], controllers:{
+    sessions: "admin/sessions"
+  }
+  
+  #管理者用
+  devise_for :admins
+#会員用
+  devise_for :users
+  get 'homes/top'
+    root to: "homes#top"
+end
--- db/migrate/20230311015324_create_sweets.rb
+++ db/migrate/20230311015324_create_sweets.rb
@@ -1,7 +1,10 @@
 class CreateSweets < ActiveRecord::Migration[6.1]
   def change
     create_table :sweets do |t|
-
+      t.integer :tag_id
+      t.string :name
+      t.text :description
+      t.string :image_id
       t.timestamps
     end
   end

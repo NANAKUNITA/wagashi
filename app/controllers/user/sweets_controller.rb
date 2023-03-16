1033fc98194a71c4121aea81e24ca5e798bb0da6
EC2 Default User <ec2-user@ip-172-31-44-206.ap-northeast-1.compute.internal>
2023-03-16 08:56:22 +0000

    [Update]

--- app/controllers/user/sweets_controller.rb
+++ app/controllers/user/sweets_controller.rb
@@ -1,2 +1,12 @@
 class User::SweetsController < ApplicationController
+     #before_action :authenticate_user!
+     
+     def index
+         @sweets=Sweet.page(params[:page]).per(36) 
+     end
+     
+     def show
+         @sweet=Sweet.find(params[:id])
+         
+     end
 end

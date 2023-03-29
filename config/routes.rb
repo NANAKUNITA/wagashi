7ac55d38d3cebc65dac729e314691494d47ef54f
EC2 Default User <ec2-user@ip-172-31-44-206.ap-northeast-1.compute.internal>
2023-03-29 12:54:45 +0000

    [Update]

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,9 @@ devise_for :users, skip:[:passwords], controllers:{
     scope module: :user do
     root 'homes#top'
     resources :users, only: [:index, :show]
-    resources :posts, only: [:show, :create, :new, :update, :edit, :destroy] #indexは、 root 'homes#top'のため、ここでは指定しない
+    resources :posts, only: [:show, :create, :new, :update, :edit, :destroy] do #indexは、 root 'homes#top'のため、ここでは指定しない
+       resources :comments, only: [:create, :destroy] 
+     end
     resource :favorites, only: [:create, :destroy]#resourceは、単数形にすると、/:idがURLに含まれなくなる。
     resources :comments, only: [:create, :destroy]
     patch '/users/withdraw' => 'users#withdraw'
@@ -23,7 +25,11 @@ end
   #管理者側
   namespace :admin do
      root 'homes#top'
-     resources :posts, only: [:show, :destroy] #indexは、 root 'homes#top'のため、ここでは指定しない
-     resources :users
+     resources :posts, only: [:show, :destroy] #do #indexは、 root 'homes#top'のため、ここでは指定しない
+        #resources :comments, only: [:create, :destroy] #書き直し！！
+     #end
+     resources :users, only: [:index, :show]
+     resources :sweets, only: [:show, :index, :new, :create, :edit, :update]
+     resources :genres, only: [:index, :create, :edit, :update]
 end
 end

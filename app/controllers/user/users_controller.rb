417238051ad596af8ffe62ec1a12dffa903af666
EC2 Default User <ec2-user@ip-172-31-44-206.ap-northeast-1.compute.internal>
2023-04-02 14:52:15 +0000

    [Update]

--- app/controllers/user/users_controller.rb
+++ app/controllers/user/users_controller.rb
@@ -1,7 +1,10 @@
 class User::UsersController < ApplicationController
   def show
     @user=User.find(params[:id])
-    @posts=@user.posts.page(params[:page])
+    @posts=@user.posts
+    
+    favorites=Favorite.where(user_id: current_user.id).pluck(:post_id)
+    @favorite_list=Favorite.find(favorites)
   end
   
   def edit

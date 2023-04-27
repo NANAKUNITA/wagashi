f3d6dd84f4aba42116d7a6a338ce599979bc2ea1
EC2 Default User <ec2-user@ip-172-31-44-206.ap-northeast-1.compute.internal>
2023-04-27 11:26:53 +0000

    [Fix] model

--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -1,5 +1,5 @@
-#class Hashtag < ApplicationRecord
+class Hashtag < ApplicationRecord
   #validates :hashname, presence: true, length: { maximum:99}
   #has_many :hashtag_post_relations
   #has_many :posts, through: :hashtag_post_relations
-#end
+end
--- app/models/hashtag_post.rb
+++ app/models/hashtag_post.rb
@@ -1,6 +1,6 @@
-#class PostHashtag < ApplicationRecord
+class PostHashtag < ApplicationRecord
   #belongs_to :post
   #belongs_to :hashtag
   #validates  :post_id, presence: true
   #validates  :Hashtag_id,   presence: true
-#end
+end
